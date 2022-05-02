58a1b793e5d51b9e0485e4b24a80076c1486d1f5
Ricardo Dematte <103331571+ricardodematte@users.noreply.github.com>
2022-05-02 19:21:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Média aritmética de 3 notas
+
+import os
+n1 = (float(input("Digite um número inteiro n1:\n")))
+n2 = (float(input("Digite um número inteiro n2:\n")))
+n3 = (float(input("Digite um número inteiro n3:\n")))
+media = (n1+n2+n3)/3
+os.system("clear")
+print ("A média é ",media)
